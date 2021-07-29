fb4a6b95db3d5a402d17610bc00d1acc0c716dac
me-sajib <me.mrsajib@gmail.com>
2021-07-29 20:56:57 +0600

    true or false check

---
+++ true-false.js
@@ -0,0 +1,15 @@
+// false value is 
+// null
+// NaN
+// false
+// undefined
+
+// truthy value
+// '0' and ' ' []
+
+const name = [];
+if (name) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
