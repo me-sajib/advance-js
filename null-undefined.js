61c84294a40a76a4050ab5b9ca2baadb4e6e4edd
me-sajib <me.mrsajib@gmail.com>
2021-07-29 21:13:04 +0600

    null or undefined value

---
+++ null-undefined.js
@@ -0,0 +1,14 @@
+// function name (){
+//     console.log('all name is here');
+// }
+// const result = name();
+// console.log(result);//this result variable is undefined
+// const age = [3, 23, 32, 54, 32];
+// console.log(age[11]);//no index count is 11 , that's why is a undefined value 
+
+// const address = {id:231, name:'sajib'};
+// console.log(address.home);//home is not defined in our object that's why is undefined value
+
+let car = "audi"
+car = null
+console.log(car);//set car call null value that's why it's a null value 
