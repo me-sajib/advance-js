7fba1c87c9546586e06e2e7493b616736d535240
me-sajib <me.mrsajib@gmail.com>
2021-07-30 10:28:51 +0600

    double vs triple equle

---
+++ double-vs-triple-equel.js
@@ -0,0 +1,12 @@
+// const first = 0;
+// const second = false;
+// const first = 1;
+// const second = true;
+const first = 0;
+const second = '0';
+if(first === second){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
